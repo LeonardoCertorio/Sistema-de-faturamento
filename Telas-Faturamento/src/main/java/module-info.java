19ea0592aff100b014d7ccbc88bd8b66ae70401b
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-03-30 21:00:49 -0300

    Delete to change by new

--- Telas-Faturamento/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.telasfaturamento {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.telasfaturamento to javafx.fxml;
-    exports com.example.telasfaturamento;
-}
